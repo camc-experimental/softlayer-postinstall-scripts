6b0e40e3bd8ca52af8540f2e782a056c67dcb195
Hongwei Tian <htian@us.ibm.com>
2017-01-26 15:48:23 -0600

    Initial commit for strongloop, nodejs, angularjs and mongodb installation scripts

---
+++ strongloop-three-tiers/install_strongloop_nodejs_in_centos_7.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#################################################################
+# Script to install NodeJS and StrongLoop 
+#
+#################################################################
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+logfile="/var/log/install_strongloop_nodejs.log"
+
+#update
+
+echo "---update system---" | tee -a $logfile 2>&1 
+yum update -y >> $logfile 2>&1 
+
+#install node.js
+
+echo "---start installing node.js---" | tee -a $logfile 2>&1 
+yum install epel-release -y >> $logfile 2>&1 || { echo "---Failed to install epel---" | tee -a $logfile; exit 1; }
+yum install nodejs -y >> $logfile 2>&1 || { echo "---Failed to install node.js---"| tee -a $logfile; exit 1; }
+echo "---finish installing node.js---" | tee -a $logfile 2>&1 
+
+#install strongloop
+
+echo "---start installing strongloop---" | tee -a $logfile 2>&1 
+yum groupinstall 'Development Tools' -y >> $logfile 2>&1 || { echo "---Failed to install development tools---" | tee -a $logfile; exit 1; }
+npm install -g strongloop >> $logfile 2>&1 || { echo "---Failed to install strongloop---" | tee -a $logfile; exit 1; }
+echo "---finish installing strongloop---" | tee -a $logfile 2>&1 
