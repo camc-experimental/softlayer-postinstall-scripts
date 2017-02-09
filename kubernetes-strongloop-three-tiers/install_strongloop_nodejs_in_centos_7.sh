74cec3b9e48a6d92cca55191bd6412e1c906d51d
Hongwei Tian <htian@us.ibm.com>
2017-02-09 16:18:54 -0600

    Simplify for testing

--- kubernetes-strongloop-three-tiers/install_strongloop_nodejs_in_centos_7.sh
+++ kubernetes-strongloop-three-tiers/install_strongloop_nodejs_in_centos_7.sh
@@ -18,25 +18,26 @@ yum update -y >> $logfile 2>&1
 #install node.js
 
 echo "---start installing node.js---" | tee -a $logfile 2>&1 
-yum install epel-release -y >> $logfile 2>&1 || { echo "---Failed to install epel---" | tee -a $logfile; exit 1; }
-yum install nodejs -y >> $logfile 2>&1 || { echo "---Failed to install node.js---"| tee -a $logfile; exit 1; }
+#yum install epel-release -y >> $logfile 2>&1 || { echo "---Failed to install epel---" | tee -a $logfile; exit 1; }
+#yum install nodejs -y >> $logfile 2>&1 || { echo "---Failed to install node.js---"| tee -a $logfile; exit 1; }
 echo "---finish installing node.js---" | tee -a $logfile 2>&1 
 
 #install strongloop
 
 echo "---start installing strongloop---" | tee -a $logfile 2>&1 
-yum groupinstall 'Development Tools' -y >> $logfile 2>&1 || { echo "---Failed to install development tools---" | tee -a $logfile; exit 1; }
-npm install -g strongloop >> $logfile 2>&1 || { echo "---Failed to install strongloop---" | tee -a $logfile; exit 1; }
+#yum groupinstall 'Development Tools' -y >> $logfile 2>&1 || { echo "---Failed to install development tools---" | tee -a $logfile; exit 1; }
+#npm install -g strongloop >> $logfile 2>&1 || { echo "---Failed to install strongloop---" | tee -a $logfile; exit 1; }
 echo "---finish installing strongloop---" | tee -a $logfile 2>&1 
 
 #install sample application
 
 
 echo "---start installing sample application---" | tee -a $logfile 2>&1 
-
+MongoDBServer=$1
 MongoDB_Server=$1
 DBUserPwd=$2	
 
+echo "---MongoDBServer: $MongoDBServer---" | tee -a $logfile 2>&1 
 echo "---MongoDB_Server: $MongoDB_Server---" | tee -a $logfile 2>&1 
 echo "---DBUserPwd: $DBUserPwd---" | tee -a $logfile 2>&1 
 sleep infinity
