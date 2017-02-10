7457f643f9a5fc762cc3dfe1c9483da827874b26
Hongwei Tian <htian@us.ibm.com>
2017-02-10 06:13:14 -0600

    /usr/bin/slc

--- kubernetes-strongloop-three-tiers/install_strongloop_sample.sh
+++ kubernetes-strongloop-three-tiers/install_strongloop_sample.sh
@@ -23,7 +23,7 @@ tar -xzvf $WORKDIR/sample-application.tar.gz -C $WORKDIR >> $logfile 2>&1 || { e
 
 sed -i -e "s/mongodb-server/$MongoDB_Server/g" $WORKDIR/strongloop-sample/server/datasources.json >> $logfile 2>&1 || { echo "---Failed to configure datasource with mongodb server address---" | tee -a $logfile; exit 1; }
 sed -i -e "s/sampleUserPwd/$DBUserPwd/g" $WORKDIR/strongloop-sample/server/datasources.json >> $logfile 2>&1 || { echo "---Failed to configure datasource with mongo user password---" | tee -a $logfile; exit 1; }
-slc run $WORKDIR/strongloop-sample & >> $logfile 2>&1 || { echo "---Failed to start the application---" | tee -a $logfile; exit 1; }
+/usr/bin/slc run $WORKDIR/strongloop-sample & >> $logfile 2>&1 || { echo "---Failed to start the application---" | tee -a $logfile; exit 1; }
 
 echo "---finish installing strongloop sample application---" | tee -a $logfile 2>&1 
 exit 0
