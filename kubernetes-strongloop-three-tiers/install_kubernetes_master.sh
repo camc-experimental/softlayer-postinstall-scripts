01cafaa5687afd70f7df7e533ee3b50bfc4d9a59
Hongwei Tian <htian@us.ibm.com>
2017-02-11 12:14:37 -0600

    Number of strongloop/angular containers is the same as that of minions

--- kubernetes-strongloop-three-tiers/install_kubernetes_master.sh
+++ kubernetes-strongloop-three-tiers/install_kubernetes_master.sh
@@ -192,6 +192,8 @@ kubectl create -f todolist-mongodb-service.yaml | tee -a $LOGFILE 2>&1
 # create a todolist-strongloop deployment
 #################################################################
 
+Count=$(cat userdata/meta.js | python -c 'import json,sys; unwrap1=json.load(sys.stdin)[0]; map=json.loads(unwrap1); print map["count"];')
+
 echo "---create a replication controller for todolist-strongloop---" | tee -a $LOGFILE 2>&1
 cat << 'EOF' > todolist-strongloop-deployment.yaml
 apiVersion: extensions/v1beta1
@@ -199,7 +201,7 @@ kind: Deployment
 metadata:
   name: todolist-strongloop-deployment
 spec:
-  replicas: 2
+  replicas: $Count
   template:
     metadata:
       labels:
@@ -284,7 +286,7 @@ kind: Deployment
 metadata:
   name: todolist-angularjs-deployment
 spec:
-  replicas: 2
+  replicas: $Count
   template:
     metadata:
       labels:
