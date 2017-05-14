6c0d7da8aebbbd1b0ed0095c3d54521d5129ddc3
Hongwei Tian <htian@us.ibm.com>
2017-05-13 21:21:33 -0500

    Remove eth1 and add new default gw in eth0 in vm2 case

--- networking/workaround.sh
+++ networking/workaround.sh
@@ -6,8 +6,16 @@ mkdir userdata
 mount /dev/xvdh1 userdata 
 cd userdata
 
-GATEWAY=$(cat meta.js | python -c 'import json,sys; unwrap1=json.load(sys.stdin)[0]; map=json.loads(unwrap1); print map["gateway"];')
+PRIVATE_GATEWAY=$(cat meta.js | python -c 'import json,sys; unwrap1=json.load(sys.stdin)[0]; map=json.loads(unwrap1); print map["gateway"];')
 EXAMPLE_VM_NUMBER=$(cat meta.js | python -c 'import json,sys; unwrap1=json.load(sys.stdin)[0]; map=json.loads(unwrap1); print map["example_vm_number"];')
 
-echo $GATEWAY >> /root/sample.txt
+#OLD_PRIVATE_GATEWAY=$(ip route show default | grep 10.0.0.0/8 | awk {'print $3'})
+
+if [ $EXAMPLE_VM_NUMBER == "vm3" ]; then
+	sed -i -e 's/ONBOOT=yes/ONBOOT=no/g' /etc/sysconfig/network-scripts/ifcfg-eth1
+	echo "GATEWAY=$PRIVATE_GATEWAY" >> /etc/sysconfig/network-scripts/ifcfg-eth0	
+	service network restart
+fi
+
+echo $PRIVATE_GATEWAY >> /root/sample.txt
 echo $EXAMPLE_VM_NUMBER >> /root/sample.txt
