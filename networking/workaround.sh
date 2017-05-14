b58defc02ca5682c2705d21c5658d882d1ce9df7
Hongwei Tian <htian@us.ibm.com>
2017-05-13 22:12:50 -0500

    Add route to vpn subnet in vm1 case

--- networking/workaround.sh
+++ networking/workaround.sh
@@ -9,13 +9,15 @@ cd userdata
 PRIVATE_GATEWAY=$(cat meta.js | python -c 'import json,sys; unwrap1=json.load(sys.stdin)[0]; map=json.loads(unwrap1); print map["gateway"];')
 EXAMPLE_VM_NUMBER=$(cat meta.js | python -c 'import json,sys; unwrap1=json.load(sys.stdin)[0]; map=json.loads(unwrap1); print map["example_vm_number"];')
 
-#OLD_PRIVATE_GATEWAY=$(ip route show default | grep 10.0.0.0/8 | awk {'print $3'})
+if [ $EXAMPLE_VM_NUMBER == "vm3" ]; then
+	VPN_SUBNET=$(cat meta.js | python -c 'import json,sys; unwrap1=json.load(sys.stdin)[0]; map=json.loads(unwrap1); print map["vpn_subnet"];')
+	echo "$VPN_SUBNET via $PRIVATE_GATEWAY" >> /etc/sysconfig/network-scripts/route-eth0
+	service network restart
+fi
+
 
 if [ $EXAMPLE_VM_NUMBER == "vm3" ]; then
 	sed -i -e 's/ONBOOT=yes/ONBOOT=no/g' /etc/sysconfig/network-scripts/ifcfg-eth1
 	echo "GATEWAY=$PRIVATE_GATEWAY" >> /etc/sysconfig/network-scripts/ifcfg-eth0	
 	service network restart
 fi
-
-echo $PRIVATE_GATEWAY >> /root/sample.txt
-echo $EXAMPLE_VM_NUMBER >> /root/sample.txt
